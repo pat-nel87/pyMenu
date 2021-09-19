09f49a7c48a0a54f3f5e937ad935c7f6aeaa8345
pat-nel87 <patnelson2022@gmail.com>
2021-09-18 21:06:57 -0400

    Working on cool terminal menu with rich library

--- pyMenu.py
+++ pyMenu.py
@@ -1,14 +1,52 @@
+# Patrick Nelson's fedora 34 menu script
+# executes on launch of bash terminal
+import os
 from rich import print
 from rich.console import Console
 from time import sleep
 
-console = Console()
-print("[bold red]Hello[/bold red] ", "[bold green]Patrick[/bold green]", ":vampire:")
+# A cute little menu script 
+# to help speed up access
+# to commonly used scripts / applications
+# Script is designed to detect any bash scripts
+# in my home directory and add them to a menu
+# where you can choose to launch the script
+# by choosing from menu list
 
+console = Console()
+# a kitchy status bar effect just for fun
 with console.status("[bold cyan]Scanning Terminal Environment...") as status:
-        timer = 1
+        timer = 2
         while (timer > 0):
             sleep(1)
             timer = timer - 1
 
-print("[bold cyan] Welcome to your Terminal,[/bold cyan] [bold green] Dude![/bold green]", ":smiley:", ":raccoon:")
+x = os.listdir(r"/home/sav-dab87/")
+console.print("[bold cyan]Welcome to your Terminal,[/bold cyan] [bold green] Dude![/bold green]", ":smiley:", ":raccoon:",justify="center")
+console.print("[bold green]Terminal Scripts List[/bold green]",justify="center")
+
+# a dictionary to hold a numeric key associated with each
+# script file to allow user to choose script to launch by number
+
+dictList = {}
+j=1
+for i in x:
+    #searches home directory for .sh files
+    if i.endswith(".sh"):
+       console.print(str(j) + "." + i, justify="center")
+       dictList.update({str(j): i})
+       j+=1
+# Prompts user for choice 
+choice = input("Enter a number to launch script or 0 to quit to terminal---> ")
+numChoice = int(choice)
+if (choice == 0):
+    quit()
+elif (numChoice > 0):
+    os.system("./" + dictList.get(choice))
+
+
+
+#print(f"{dictList}")
+
+
+
